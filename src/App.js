2ba785d85df73fbab77a365a03e1be2635a51da4
Captain-ss <swadeshishu2001@gmail.com>
2024-01-26 16:21:43 +0530

    table setup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,147 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Form from "./pages/Form";
 import { Routes, Route } from "react-router-dom";
 import NotfoundPage from "./pages/NotfoundPage";
-import DataSet from "./store/DataSetSlice/DataSet";
+import { Dataset } from "./pages/Dataset";
 const queryClient = new QueryClient();
 
-
+const dataSet = [
+  {
+    name: "Tiger Nixon",
+    position: "System Architect",
+    salary: "$3,120",
+    start_date: "2011/04/25",
+    office: "Edinburgh",
+    extn: "5421",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+  {
+    name: "Garrett Winters",
+    position: "Director",
+    salary: "$5,300",
+    start_date: "2011/07/25",
+    office: "Edinburgh",
+    extn: "8422",
+  },
+];
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -14,7 +151,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Form />} />
           <Route path="/Form" element={<Form />} />
-          <Route path="/table" element={<DataSet />} />
+          <Route path="/table" element={<Dataset data={dataSet} />} />
           <Route path="*" element={<NotfoundPage />} />
         </Routes>
       </div>
--- src/pages/Dataset.js
+++ src/pages/Dataset.js
@@ -1,12 +1,65 @@
-import React from 'react'
-import DataTable from 'datatables.net-dt';
-let table = new DataTable('#myTable');
-const Dataset = () => {
-  return (
-    <div>
-      <DataTable/>
-    </div>
-  )
-}
+import React, { Component } from "react";
+import "../CSS/DataSetPage.css";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+const $ = require("jquery");
+$.DataTable = require("datatables.net");
+export class Dataset extends Component {
+  
+  componentDidMount() {
+    var buttonCommon = {
+      exportOptions: {
+          format: {
+              body: function ( data, row, column, node ) {
+                  // Strip $ from salary column to make it numeric
+                  return column === 5 ?
+                      data.replace( /[$,]/g, '' ) :
+                      data;
+              }
+          }
+      }
+    };
+    console.log(this.el);
+    this.$el = $(this.el);
+    this.$el.DataTable({
+      data: this.props.data,
+      columns: [
+        { data: "name" },
+        { data: "position" },
+        { data: "salary" },
+        { data: "office" },
+      ],
+      
+      buttons: [
+        $.extend(true, {}, buttonCommon, {
+          extend: "copyHtml5",
+        }),
+        $.extend(true, {}, buttonCommon, {
+          extend: "excelHtml5",
+        }),
+        $.extend(true, {}, buttonCommon, {
+          extend: "pdfHtml5",
+        }),
+      ],
+    });
+  }
 
-export default Dataset
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <div className="dataSet_container">
+        <table className="display" width="100%" ref={(el) => (this.el = el)}>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>position</th>
+              <th>salary</th>
+              <th>office</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    );
+  }
+}
